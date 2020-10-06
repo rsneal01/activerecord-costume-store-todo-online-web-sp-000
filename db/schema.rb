fa20913aa1679ce85bd447bb29050f5acdbbcf7d
Bobby Neal <rsneal01@gmail.com>
2020-10-06 18:18:27 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.float "price"
-    t.text "url"
+    t.text "image_url"
     t.string "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
